7d63eec567fb0785f2f98e52c678f27d6432ab3d
JakubStolarski <p.jakub.stolarski@gmail.com>
2023-11-09 16:26:26 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='baddie-botbowl',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/Colty1999/baddie-botbowl',
+    license='',
+    author='kubas',
+    author_email='',
+    description=''
+)
