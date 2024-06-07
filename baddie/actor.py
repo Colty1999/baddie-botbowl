48052fb38ef29d102e4c29d0a6f29b1e4200335b
JakubStolarski <p.jakub.stolarski@gmail.com>
2024-06-07 12:42:50 +0200

    Update actor and add test games to actor.py

--- baddie/actor.py
+++ baddie/actor.py
@@ -1,43 +1,40 @@
 import numpy as np
 import ray
 import torch
+from torch.autograd import Variable
+from typing import Callable
 
 import botbowl
-from botbowl.ai.env import EnvConf
+from botbowl.ai.env import EnvConf, BotBowlEnv
+from botbowl.ai.layers import *
+import botbowl.web.server as server
 
-from reinforced_enemy.reinforced_agent import make_env, ConfigParams  # todo Make specific Conffig Params for DQN
-from network import CNNPolicy
+from baddie.network import CNNPolicy, ConfigParams
+from Data.scripted_bot import ScriptedBot
 
 
-@ray.remote
+# @ray.remote
 class BaddieBotActor(botbowl.Agent):
+    env: BotBowlEnv
     BOT_ID = 'Baddie'
 
     def __init__(self, name='BaddieBot', env_conf: EnvConf = EnvConf(size=11, pathfinding=True),
+                 scripted_func: Callable[[Game], Optional[Action]] = None,
                  filename=ConfigParams.model_path.value):
         super().__init__(name)
+        self.env = BotBowlEnv(env_conf)
 
+        self.scripted_func = scripted_func
         self.action_queue = []
-        self.gamma = ConfigParams.gamma.value
-        # todo make variables below configurable
-        self.worker_buffer_size = 1000
-        self.eps_greedy = 0.3
-        self.eps_decay = 0.95
 
-        self.env = botbowl.BotBowlEnv(env_conf)
-        self.env.reset()
-
-        spatial_obs, non_spatial_obs, action_mask = self.env.get_state()
-        spatial_shape = spatial_obs.shape
-        num_non_spatial = non_spatial_obs.shape[0]
-        num_actions = len(action_mask)
-
-        self.model = CNNPolicy(spatial_shape, num_non_spatial)
+        # MODEL
+        # self.policy = CNNPolicy()  # For testing games
+        # self.policy.load_state_dict(torch.load(filename))
         self.device = ConfigParams.device.value
-        if model_path:
-            self.model.load_state_dict(torch.load(model_path, map_location=self.device))
-            self.model.eval()  # activate evaluation mode
-        self.model.to(self.device)
+        self.policy = torch.load(filename)
+        self.policy.eval()
+        self.policy.to(self.device)
+        self.end_setup = False
 
     def new_game(self, game, team):
         '''
@@ -46,6 +43,10 @@ class BaddieBotActor(botbowl.Agent):
         self.own_team = team
         self.opp_team = game.get_opp_team(team)
         self.is_home = team == game.state.home_team
+
+    @staticmethod
+    def _update_obs(array: np.ndarray):
+        return torch.unsqueeze(torch.from_numpy(array.copy()), dim=0)
 
     def sample(self, state):
         self.eps_greedy = self.eps_greedy * self.eps_decay
@@ -62,25 +63,30 @@ class BaddieBotActor(botbowl.Agent):
         return actions
 
     def act(self, game):
-        '''
-        Called for every game step in order to determine the action to take.
-        '''
         if len(self.action_queue) > 0:
             return self.action_queue.pop(0)
 
+        if self.scripted_func is not None:
+            scripted_action = self.scripted_func(game)
+            if scripted_action is not None:
+                return scripted_action
+
         self.env.game = game
-        spatial_obs, non_spatial_obs, action_mask = self.env.get_state()
-        spatial_obs = torch.from_numpy(np.stack(spatial_obs)[np.newaxis]).float().to(self.device)
-        non_spatial_obs = torch.from_numpy(np.stack(non_spatial_obs)[np.newaxis]).float().to(self.device)
-        action_mask = torch.tensor(action_mask, dtype=torch.bool).to(self.device)
 
-        _, actions = self.model.act(spatial_obs, non_spatial_obs, action_mask)
-        # Create action from output
+        spatial_obs, non_spatial_obs, action_mask = map(BaddieBotActor._update_obs, self.env.get_state())
+        spatial_obs = spatial_obs.to(ConfigParams.device.value)
+        non_spatial_obs = torch.unsqueeze(non_spatial_obs, dim=0)
+        non_spatial_obs = non_spatial_obs.to(ConfigParams.device.value)
+        action_mask = action_mask.to(ConfigParams.device.value)
+
+        _, actions = self.policy.act(
+            Variable(spatial_obs.float()),
+            Variable(non_spatial_obs.float()),
+            Variable(action_mask))
+
         action_idx = actions[0]
+        action_objects = self.env._compute_action(action_idx)
 
-        action_objects = self.env._compute_action(action_idx.cpu().numpy()[0], flip=not self.is_home)
-
-        # Return action to the framework
         self.action_queue = action_objects
         return self.action_queue.pop(0)
 
@@ -105,3 +111,67 @@ class BaddieBotActor(botbowl.Agent):
         """
         if BaddieBotActor.BOT_ID.lower() not in botbowl.list_bots():
             botbowl.register_bot(BaddieBotActor.BOT_ID, lambda name: BaddieBotActor(name=name, model_path=path))
+
+def main():
+    # Register the bot to the framework
+    def _make_my_a2c_bot(name, env_size=11):
+        return BaddieBotActor(name=name,
+                        env_conf=EnvConf(size=env_size),
+                        # scripted_func=a2c_scripted_actions,
+                        filename=ConfigParams.agent_path.value)
+    botbowl.register_bot('my-a2c-bot', _make_my_a2c_bot)
+    botbowl.register_bot('scripted', ScriptedBot)
+
+    # Load configurations, rules, arena and teams
+    config = botbowl.load_config("bot-bowl")
+    config.competition_mode = False
+    config.pathfinding_enabled = False
+    ruleset = botbowl.load_rule_set(config.ruleset)
+    arena = botbowl.load_arena(config.arena)
+    home = botbowl.load_team_by_filename("human", ruleset)
+    away = botbowl.load_team_by_filename("human", ruleset)
+    config.competition_mode = False
+    config.debug_mode = False
+
+    # Play 10 games
+    wins = 0
+    draws = 0
+    n = 25
+    is_home = True
+    tds_away = 0
+    tds_home = 0
+    for i in range(n):
+        # host = "127.0.0.1"
+        # server.start_server(host=host, debug=True, use_reloader=False, port=1234)
+        if is_home:
+            away_agent = botbowl.make_bot('random')
+            home_agent = botbowl.make_bot('my-a2c-bot')
+        else:
+            away_agent = botbowl.make_bot('my-a2c-bot')
+            home_agent = botbowl.make_bot("random")
+        game = botbowl.Game(i, home, away, home_agent, away_agent, config, arena=arena, ruleset=ruleset)
+        game.config.fast_mode = True
+
+        print("Starting game", (i+1))
+        game.init()
+        print("Game is over")
+
+        winner = game.get_winner()
+        print(winner)
+        if winner is None:
+            draws += 1
+        elif winner == home_agent and is_home:
+            wins += 1
+        elif winner == away_agent and not is_home:
+            wins += 1
+
+        tds_home += game.get_agent_team(home_agent).state.score
+        tds_away += game.get_agent_team(away_agent).state.score
+
+    print(f"Home/Draws/Away: {wins}/{draws}/{n-wins-draws}")
+    print(f"Home TDs per game: {tds_home/n}")
+    print(f"Away TDs per game: {tds_away/n}")
+
+
+if __name__ == "__main__":
+    main()
